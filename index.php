0b033e2cc9fff11f3225973d56f5ed6473e82d2d
toto toto <eboutiquefacile@gmail.com>
2020-02-27 10:24:28 +0100

    poo test2

--- index.php
+++ index.php
@@ -13,12 +13,34 @@ class Employe
      * @var string
      */
     public $nom;
+
+    /**
+     * Une méthode est une fonction interne à la classe
+     * @return string
+     */
+
+    public function nomComplet()
+    {
+        // $this fait référence à l'objet instance de la classe qui appelle la méthode
+        return $this->prenom . ' ' . $this->nom;
+    }
 }
+//----------------------------------------------------------------------------------------------------------------------
 
 // instanciation de la classe
 // $moi est un objet instance de la classe Employé
 $moi = new Employe();
 
 // la flèche permet d'accèder à l'attribut
-echo $moi->prenom;
-// ezqr
+$moi->nom = 'Faton';
+
+echo $moi->prenom . ' '; echo $moi->nom;
+var_dump($moi->prenom, $moi->nom);
+
+
+$toi = new Employe();
+$toi->prenom = 'Ben';
+var_dump($moi, $toi);
+
+// appel de la méthode nomComplet()
+echo $moi->nomComplet();
