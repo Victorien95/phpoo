93ccd7e3f77c69fd592b8e9b4931c21b9dcd3888
toto toto <eboutiquefacile@gmail.com>
2020-02-27 10:01:31 +0100

    poo test

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+class Employe
+{
+    /**
+     * attribut de classe avec une valeur par défaut
+     * @var string
+     */
+    public $prenom = 'Victorien';
+
+
+    /**
+     *  attribut de classe sans valeur par défaut (=null)
+     * @var string
+     */
+    public $nom;
+}
+
+// instanciation de la classe
+// $moi est un objet instance de la classe Employé
+$moi = new Employe();
+
+// la flèche permet d'accèder à l'attribut
+echo $moi->prenom;
