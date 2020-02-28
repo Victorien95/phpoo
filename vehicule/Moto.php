d4c1f84efc28f70bbff96823dec78a16343430b4
toto toto <eboutiquefacile@gmail.com>
2020-02-28 17:01:29 +0100

    Maj

--- vehicule/Moto.php
+++ vehicule/Moto.php
@@ -19,19 +19,19 @@ class Moto extends Vehicule
     {
         if ($this->getCarburant() != $pompe->getCarburant())
         {
-            echo "Vous ne pouvez pas faire le plein à cette pompe ! Le carburant ne correspond pas";
+            echo "Vous ne pouvez pas faire le plein à cette pompe " . $this->getNom() ." ! Le carburant ne correspond pas <br>";
             return $this;
         }
-        if ($pompe->getContenueEssence() == 0)
+        if ($pompe->getContenueEssence() <= 0)
         {
-            echo "La pompe est vide";
+            echo "La pompe est vide <br>";
             return $this;
         }
-        $this->setCarburant(10);
-        $pompe->setCarburant(-10);
-        if ($this->getReservoirContenu() == $this->getReservoirContenance())
+        $this->setReservoirContenu($this->getReservoirContenu() + 20);
+        $pompe->setContenueEssence($pompe->getContenueEssence() - 20);
+        if ($this->getReservoirContenu() >= $this->getReservoirContenance())
         {
-            echo 'Message De la pompe ' . $pompe->getNom();
+            echo '<br>Message De la pompe ' . $pompe->getNom;
             echo $this->getNom() . ' A fait le plein <br>';
         }
         echo $this->getNom() . ' est à la pompe et ajoute 20 L <br>';
